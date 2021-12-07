16e223e93c97eec237888ea55117c2d3fe7e5dbc
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2021-12-07 15:27:27 -0700

    declare winner

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,14 +1,9 @@
-class Square extends React.Component {
-    render() {
-      return (
-        <button
-          className="square"
-          onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
@@ -16,13 +11,20 @@ class Square extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
   
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   
     renderSquare(i) {
@@ -35,19 +37,31 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
           </div>
         </div>
       );
@@ -76,4 +90,24 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
